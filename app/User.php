ab5f6c04bb0380948e5dcb19beedf0ab53edc9d3
Evelio Mora <evelio@evelio.eve>
2019-06-08 17:37:54 +0200

    Start

---
+++ app/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+<<<<<<< HEAD:ambulancia/app/User.php
+        'name', 'email', 'password',
+=======
+        'name',
+        'email',
+        'password',
+        'status',
+        'is_active',
+        
+>>>>>>> fa8a15f322b014bdc2fafdb942cfdb9910ce15e8:app/User.php
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
