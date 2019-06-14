98bd1e44aa49e76f7d7d530c107b0951d12e04e9
Javier <izquierdoj98@gmail.com>
2019-06-14 14:34:05 +0200

    WIP Image carousel

---
+++ resources/lang/es/index.php
@@ -0,0 +1,6 @@
+<?php 
+return array ( 
+    'key' => 'value',
+    'hola' => 'hola pero en espanol',
+    'hello' => '<h2>hello encabezado</h2>'
+);
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Index</h1>
-    {{App::getLocale()}}
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Ambulancia del Deseo</title>
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="arousel-inner" role="listbox">
+                <div class="item active">
+                <img height="auto" width="auto" src="https://ambulanciadeldeseo.es/wp-content\uploads\2018\10\banner-contacto-ambulancia-del-deseo.jpg">
+                <div class="carousel-caption">
+                    <h1>text</h1>
+                    <br>
+                    <button type="button" class="btn btn-default">text2</button>
+            </div>
+        </div>
+
+
+        <!-- 
+        <h1>Index</h1>
+        {!! __('index.hello')!!}
+        @lang('index.hola')
+        -->
+    </body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
